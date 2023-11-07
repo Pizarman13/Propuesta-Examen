c261d4f074f771a6c7b80e47b3027d9547bfeda5
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2023-11-07 11:44:37 +0100

    Comit final

--- main.cpp
+++ main.cpp
@@ -5,17 +5,22 @@ void encontrarTriplesPitagoricos(int limite) {
     int a, b, c;
     for (a = 1; a <= limite; a++) {
         for (b = a; b <= limite; b++) {
+            // Calcula el tercer lado del triángulo (c) usando el teorema de Pitágoras
             c = sqrt(a*a + b*b);
-            if (c <= limite && a*a + b*b == c*c) {
-                printf("- %d - %d - %d\n", a, b, c);
+            // Verifica si el triángulo satisface la condición de un triple pitagórico
+            if (c <= limite && c*c == a*a + b*b) {
+                // Verifica si los lados forman un triángulo válido
+                if ((a + b > c) && (a + c > b) && (b + c > a)) {
+                    // Imprime el triple pitagórico
+                    printf("- %d - %d - %d\n", a, b, c);
+                }
             }
         }
     }
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
 
-    encontrarTriplesPitagoricos(20);
+    encontrarTriplesPitagoricos(17);
     return 0;
 }
